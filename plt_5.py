6ff74a71ecfdce5bdb5882755eb38ed6080b3720
wln <wln@qq.com>
2018-01-02 17:04:40 +0800

    2

--- plt_5.py
+++ plt_5.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pandas as pd
+import matplotlib.pyplot as plt 
 
+review = pd.read_csv('../fandango_score_comparison.csv')
+cols = ['FILM','RT_user_norm_round','Metacritic_user_norm_round','IMDB_norm','Fandango_Ratingvalue']
+norm_re = review[cols]
+#print(norm_re[:5])
+fand_dis = norm_re['Fandango_Ratingvalue'].value_counts()
+print(fand_dis)
+fand_dis = fand_dis.sort_index()
+#print(fand_dis)
+imdb_dis = norm_re['IMDB_norm'].value_counts()
+imdb_dis = imdb_dis.sort_index()
+ax = plt.subplot()
+#ax.hist(norm_re['Fandango_Ratingvalue'])
+#ax.hist(norm_re['Fandango_Ratingvalue'],bins=21)
+ax.hist(norm_re['Fandango_Ratingvalue'],range=(4,5),bins=20)#x区间内的值(4,5)z
